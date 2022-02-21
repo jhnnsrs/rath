0bfc37bf34874196d52df878c4c82a34a48edc91
jhnnsrs <jhnnsrs@gmail.com>
2022-02-21 15:28:19 +0100

    added stuff

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/rath/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
